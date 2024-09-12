9648c099491d9aff3d92fee0dfcc289b04fd3d88
Low Kheng Liang <ict_lowkhengliang@connect.np.edu.sg>
2024-09-12 04:02:35 +0000

    Added image

---
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controllers;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//Tells springboot that this is a controller that contains route
+
+@Controller
+public class HomeController {
+
+    //When the user go to the / URL, this method will run
+    @GetMapping("/")
+    // Tell Springboot this method returns a HTTP response
+    @ResponseBody
+    public String Helloworld(){
+        return "<h1>Hello World</h1>";
+    }
+
+    @GetMapping("/about-us")
+    // If the route is not marked with responsebody, we are using templates. Return relative file path to resources/templates
+    // Model model parameter is automatically passed to aboutUs when is called by Spring. This is
+    // the view model, allow us to inject variables into our template.
+    
+    public String aboutUs(Model model){
+        LocalDateTime currentDateTime = LocalDateTime.now();
+
+        // the view model is automatically passed to the template
+        // any attributes in it will be available as variables
+
+        model.addAttribute("currentDateTime", currentDateTime);
+        return "about-us";
+    }
+}
